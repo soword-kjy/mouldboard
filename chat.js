00387bae3591a56c65b03be4dfad00bbf659e330
soword <60704502+soword-kjy@users.noreply.github.com>
2020-03-27 15:55:50 +0800

    s

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    $("body").append("<script src='https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js'></script>");
     $("body").append(" <b>追加文本</b>。");
     $("body").append("<h1>追加列表项</h1>");
   
