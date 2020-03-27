02c4ab659de208b042e4ad3cc6df8e5d09766caa
soword <60704502+soword-kjy@users.noreply.github.com>
2020-03-27 16:50:21 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,1 @@
-$(document).ready(function(){
-    $("body").append("<script src='https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js'></script>");
-    $("body").append(" <b>追加文本</b>。");
-    $("body").append("<h1>追加列表项</h1>");
-  
-});
+document.getElementsByTagName("body")[0].innerHTML="<h1>dasdas</h1>";
