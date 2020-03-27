146ecaf99a003616b64dd9dd6062b69c2f9ff1a7
soword <60704502+soword-kjy@users.noreply.github.com>
2020-03-27 14:55:22 +0800

    s

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    $("body").append("<script src='http://mouldboard.soword.cn/jquery-3.3.1.min.js'></script><li>追加列表项</li>");
+    $("body").append("<li>追加列表项</li>");
 
 });
