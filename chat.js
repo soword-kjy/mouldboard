6d4187becff956de5f1e3b31c67e5c2eef7cb598
soword <60704502+soword-kjy@users.noreply.github.com>
2020-03-27 17:27:05 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,4 @@
-document.getElementsByTagName("body")[0].innerHTML="<h1>dasdas</h1>";
+var div = document.createElement('div');
+div.innerHTML = '<h1>525</h1>';
+var bo = document.body;
+bo.insertBefore(div,bo.lastChild);
