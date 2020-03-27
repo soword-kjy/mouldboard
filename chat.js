1d1a9ef884e9bf714935db2e1e696574b206bfb1
soword <60704502+soword-kjy@users.noreply.github.com>
2020-03-27 14:50:03 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    $("body").append("<li>追加列表项</li>");
+    $("body").append("<script src="http://mouldboard.soword.cn/jquery-3.3.1.min.js"></script><li>追加列表项</li>");
 
 });
